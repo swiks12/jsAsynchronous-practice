ec1580725432ce3eae71ed74406f2ced14e6456b
Swikriti Suwal <swikriti.suwal20@gmail.com>
2024-11-20 18:01:20 +0545

    completed filter of todos

--- todoJsonServer/app.js
+++ todoJsonServer/app.js
@@ -19,16 +19,13 @@ const fetchedTodo=async()=>{
 }
 
 
-const handleComplete=(completeId)=>{
+const handleComplete=async(completeId)=>{
     const updateStatus={completed:true};
-    const response=fetch(`http://localhost:3000/todos/${completeId}`,{method:'PATCH',headers:{
+    const response=await fetch(`http://localhost:3000/todos/${completeId}`,{method:'PATCH',headers:{
         'Content-Type':'application/json'
     },
-    body:JSON.stringify(updateStatus)});
-
-    
+    body:JSON.stringify(updateStatus)}); 
 }
-
 
 
 handleDelete=(deleteId)=>{
@@ -45,69 +42,65 @@ const handleCreate=()=>{
         },
         body:JSON.stringify(newTodo)
     })
-    // if(todo!=""){
-    //     var todoArray=fetchedTodosLocalStorage();
-    //     let flag=0;
-    //     // to make sure there are no duplicate entries of todo
-    //         for (let i = 0; i < todoArray.length; i++) {
-    //             if(todo===todoArray[i]){
-    //                 // increment the value of flag when duplicacy found
-    //                 flag+=1
-    //             }
-    //         }
-    //         if (flag==0) {
-    //             todos.push(todo);
-    //         }
-    //         else{
-    //             alert("Todo Already exists! 😉")
-    //         }
-    // }
-    // else{
-    //     alert("Input your todo first! 😭")
-    // }
-    // // JSON.stringify to store the todo in more formatted way rather than plain text
-    // localStorage.setItem("todos",JSON.stringify(todos));
-    
+}
 
+
+const displayTodo=(fetchedTodos)=>{
+    // if i do not clear the inner html then jun completed array return huncha tyasmai append huncha dekhaune bela harek patak div chai clear garnu parxa
+    getTodoContainer.innerHTML=" ";
+    for (let i = 0; i < fetchedTodos.length; i++) {
+        // console.log(trackStatus.value)
+            var parentDiv=document.createElement("div");
+            var btnDiv=document.createElement("div");
+            var pEl=document.createElement("p");
+            var tickBtn=document.createElement("button");
+            var crossBtn=document.createElement("button");
+            tickBtn.innerHTML='<span class="material-symbols-outlined">check</span>';
+            crossBtn.innerHTML='<span class="material-symbols-outlined">close</span>'
+            // creating a div to put what needs to be shown in a container so that not all items are considered seperate children
+            parentDiv.classList.add("todo-parent-div")
+            btnDiv.classList.add("todo-btn-div")
+            pEl.classList.add("todo-item")
+            tickBtn.classList.add("tick-btn")
+            crossBtn.classList.add("cross-btn")
+            console.log(fetchedTodos[i].todoText)
+            pEl.innerText=fetchedTodos[i].todoText;
+            getTodoContainer.appendChild(parentDiv);
+            parentDiv.appendChild(pEl);
+            parentDiv.appendChild(btnDiv)
+            btnDiv.appendChild(tickBtn);
+            btnDiv.appendChild(crossBtn);
+    
+            crossBtn.addEventListener("click",()=>{
+                handleDelete(fetchedTodos[i].id)
+            })
+    
+            tickBtn.addEventListener("click",()=>{
+                handleComplete(fetchedTodos[i].id)
+            })
+    }
 }
 
 const getTodos=async()=>{
     var fetchedTodos=await fetchedTodo();
     console.log(fetchedTodos,"from this")
     console.log(fetchedTodos.length);
-    for (let i = 0; i < fetchedTodos.length; i++) {
-        var parentDiv=document.createElement("div");
-        var btnDiv=document.createElement("div");
-        var pEl=document.createElement("p");
-        var tickBtn=document.createElement("button");
-        var crossBtn=document.createElement("button");
-        tickBtn.innerHTML='<span class="material-symbols-outlined">check</span>';
-        crossBtn.innerHTML='<span class="material-symbols-outlined">close</span>'
-        // creating a div to put what needs to be shown in a container so that not all items are considered seperate children
-        parentDiv.classList.add("todo-parent-div")
-        btnDiv.classList.add("todo-btn-div")
-        pEl.classList.add("todo-item")
-        tickBtn.classList.add("tick-btn")
-        crossBtn.classList.add("cross-btn")
-        console.log(fetchedTodos[i].todoText)
-        pEl.innerText=fetchedTodos[i].todoText;
-        getTodoContainer.appendChild(parentDiv);
-        parentDiv.appendChild(pEl);
-        parentDiv.appendChild(btnDiv)
-        btnDiv.appendChild(tickBtn);
-        btnDiv.appendChild(crossBtn);
-        if(fetchedTodos[i].completed==true){
-            parentDiv.classList.add("todo-completed")
+    const trackStatus=document.getElementById("todo-status");
+       displayTodo(fetchedTodos);
+    var initialStatus;
+    trackStatus.addEventListener("change",()=>{
+        initialStatus=trackStatus.value;
+
+        if(initialStatus=="Completed"){       
+            const completedtodos=fetchedTodos.filter(item=>item.completed==true);
+            displayTodo(completedtodos)
         }
-
-        crossBtn.addEventListener("click",()=>{
-            handleDelete(fetchedTodos[i].id)
-        })
-
-        tickBtn.addEventListener("click",()=>{
-            handleComplete(fetchedTodos[i].id)
-        })
-    }
+        else if (initialStatus=="All") {
+            displayTodo(fetchedTodos)
+        } else {
+            displayTodo(fetchedTodos.filter(item=>item.completed==false))
+        }
+    })
 }
 getTodos();
 
