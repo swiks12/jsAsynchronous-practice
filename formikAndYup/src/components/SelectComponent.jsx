2308fcbbacdc961330268affb731a986f0d8971c
Swikriti Suwal <swikriti.suwal20@gmail.com>
2024-11-29 18:30:52 +0545

    Some changes in select component made in name attribute

--- formikAndYup/src/components/SelectComponent.jsx
+++ formikAndYup/src/components/SelectComponent.jsx
@@ -6,7 +6,7 @@ const SelectComponent = (props) => {
   return (
     <FormControl >
       <InputLabel id={props.labelId}>{props.labelText}</InputLabel>
-      <Select labelId={props.labelId} id={props.selectId} size="small" value={props.value} onChange={props.handleChange} name="selectVal" onBlur={props.handleBlur} >
+      <Select labelId={props.labelId} id={props.selectId} size="small" value={props.value} onChange={props.handleChange} name={props.name} onBlur={props.handleBlur} >
         <MenuItem value={props.option1}>{props.option1}</MenuItem>
         <MenuItem value={props.option2}>{props.option2}</MenuItem>
         <MenuItem value={props.option3}>{props.option3}</MenuItem>
