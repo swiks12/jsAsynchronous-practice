b391e60c2967970199199cb702fb73bf01276b7c
Swikriti Suwal <swikriti.suwal20@gmail.com>
2024-12-02 14:48:33 +0545

    confirm password field added along with create user functionality

--- formikAndYup/src/Login.jsx
+++ formikAndYup/src/Login.jsx
@@ -6,11 +6,11 @@ const Login = () => {
   return (
     <>
       <div className="flex justify-center items-center h-[100vh] bg-gray-100">
-        <div className="flex rounded-2xl h-[90vh]  gap-[90px] justify-center items-center pt-[80px] pl-[80px] pb-[80px] shadow-2xl  bg-white">
-          <div className="flex items-center">
+        <div className="flex rounded-2xl h-[90vh]  gap-[90px] justify-center items-center pt-[80px] pl-[100px] pb-[80px] shadow-2xl  bg-white border border-gray-200 w-[85vw]">
+          <div className="flex items-center justify-center ">
             <div>
               <p className="text-3xl font-bold mb-8 text-center">Welcome</p>
-              <form className="flex flex-col gap-4">
+              <form className="flex flex-col gap-4 w-[21vw] justify-center ">
                 {/* email */}
                 <TextFieldComponent
                   name="email"
@@ -32,6 +32,7 @@ const Login = () => {
                   // handleChange={formik.handleChange}
                   // handleBlur={formik.handleBlur}
                   type="password"
+                  // endIcon="visibility"
                   // error={formik.touched.password && formik.errors.password}
                 />
 
@@ -41,7 +42,7 @@ const Login = () => {
           </div>
           <img
             src="https://cdn.dribbble.com/userupload/16834943/file/original-f5c8959760f5125529fdc38731ff9a17.jpg?resize=1200x900&vertical=center"
-            className="h-[90vh] w-[50vw] object-cover rounded-r-2xl"
+            className="h-[90vh] w-[52vw] object-cover rounded-r-2xl"
           />
         </div>
       </div>
