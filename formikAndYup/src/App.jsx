5a6de19743bef0e62bcb51ea48bd6fc78ca64ceb
Swikriti Suwal <swikriti.suwal20@gmail.com>
2024-12-04 14:37:14 +0545

    login functionality completed with errors for invalid email, wrong password and successful login goes to home page

--- formikAndYup/src/App.jsx
+++ formikAndYup/src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import SignUp from './SignUp';
-import Login from './Login';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import { ToastContainer } from "react-toastify";
+import Home from "./Home";
+import "react-toastify/ReactToastify.css";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SignUp/>}/>
-        <Route path='/login' element={<Login/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
       </BrowserRouter>
+      <ToastContainer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
