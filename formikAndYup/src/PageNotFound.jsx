f40132b64454413ae9261ffb0820986b60155085
Swikriti Suwal <swikriti.suwal20@gmail.com>
2024-12-05 17:57:28 +0545

    page not found completed

---
+++ formikAndYup/src/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="bg-page-not-found h-[100vh] w-[100vw] bg-red-400 bg-no-repeat bg-cover"></div>
+  );
+};
+
+export default PageNotFound;
--- formikAndYup/tailwind.config.js
+++ formikAndYup/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
     extend: {
       colors:{
         'dark-purple':'#581c87'
+      },
+      backgroundImage:{
+        'page-not-found':"url('https://static.vecteezy.com/system/resources/previews/024/116/291/non_2x/utility-pole-on-dreamy-night-sky-error-404-flash-message-electrical-cable-empty-state-ui-design-lofi-background-page-not-found-cartoon-image-flat-illustration-concept-synthwave-aesthetics-vector.jpg')",
       }
     },
   },
